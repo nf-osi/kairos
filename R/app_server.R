e27e116d9bed3211c9fc56b490c4f406c16dbe43
robert allaway <robert.j.allaway@gmail.com>
2020-01-14 17:09:01 -0800

    first module

--- R/app_server.R
+++ R/app_server.R
@@ -1,4 +1,6 @@
 #' @import shiny
+#' @import shinydashboard
+
 app_server <- function(input, output,session) {
-  # List the first level callModules here
+  callModule(mod_cohort_server, "cohort_ui_1")
 }
