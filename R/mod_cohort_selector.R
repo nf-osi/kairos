1b82876eed74cc5f4ed47a03057cd5f6d3438663
robert allaway <robert.j.allaway@gmail.com>
2020-01-17 12:02:41 -0800

    tidying up

--- R/mod_cohort_selector.R
+++ R/mod_cohort_selector.R
@@ -16,17 +16,22 @@
 mod_cohort_selector_ui <- function(id){
   ns <- NS(id)
   tagList(
-    checkboxGroupInput(ns("isCellLine"), label = "Is Cell Line", choices = unique(cohort$isCellLine), 
+    # actionButton(ns('button'), label = "Build!"),
+    checkboxGroupInput(ns('isCellLine'), label = "Is Cell Line", choices = unique(cohort$isCellLine), 
                        selected = unique(cohort$isCellLine)),
-    checkboxGroupInput(ns("tumorType"), label = "Tumor Type", choices = unique(cohort$tumorType),
-                       selected =  unique(cohort$tumorType)),
-    checkboxGroupInput(ns("species"), label = "Species", choices = unique(cohort$species), 
-                       selected = unique(cohort$species)),
-    selectizeInput(ns("modelSystemName"), label = "Model System Name", choices = unique(cohort$modelSystemName), 
-                   selected = unique(cohort$modelSystemName), multiple = T),
-    selectizeInput(ns("studyName"), label = "Study Name", choices = unique(cohort$studyName),
-                   selected = unique(cohort$studyName), multiple = T)
+    # ,
+    # checkboxGroupInput(ns("tumorType"), label = "Tumor Type", choices = unique(cohort$tumorType),
+    #                    selected =  unique(cohort$tumorType)),
+    # checkboxGroupInput(ns("species"), label = "Species", choices = unique(cohort$species), 
+    #                    selected = unique(cohort$species)),
+    # selectizeInput(ns("modelSystemName"), label = "Model System Name", choices = unique(cohort$modelSystemName), 
+    #                selected = unique(cohort$modelSystemName), multiple = T),
+    # selectizeInput(ns("studyName"), label = "Study Name", choices = unique(cohort$studyName),
+    #                selected = unique(cohort$studyName), multiple = T)
+    textOutput(ns("foo"))
+  
   )
+  
 }
     
 # Module Server
@@ -38,21 +43,23 @@ mod_cohort_selector_ui <- function(id){
 mod_cohort_selector_server <- function(input, output, session){
   ns <- session$ns
   
-  specimens <- reactive({
-    
-    cohort %>% 
-    dplyr::filter(studyName %in% input$studyName,
-                    modelSystemName %in% input$modelSystemName,
-                    isCellLine %in% input$isCellLine,
-                    tumorType %in% input$tumorType,
-                    species %in% input$species) %>% 
-      purrr::pluck("specimenID") %>% 
-      unique()
-  })
+ foo <- renderText(paste0("You have chosen: ", input$isCellLine))
+
   
-  return(specimens)
+  # output$specimens <- reactive(input$isCellLine, {
+  #   print(input$isCellLine) 
+  #   kairos::cohort %>%
+  #     dplyr::filter(#studyName %in% input$studyName,
+  #                   # modelSystemName %in% input$modelSystemName,
+  #                    isCellLine %in% input$isCellLine
+  #                   # ,
+  #                   # tumorType %in% input$tumorType,
+  #                   # species %in% input$species
+  #                   ) %>%
+  #     purrr::pluck("specimenID") %>%
+  #     unique() })
   
- )}
+ }
     
 ## To be copied in the UI
 # mod_cohort_selector_ui("cohort_selector_ui_1")
