006563d003b5ef167dc38bf23a489f2e41ab2418
Robert Allaway <robert.j.allaway@gmail.com>
2020-11-23 20:21:57 -0800

    cleaning

--- R/mod_home_page.R
+++ R/mod_home_page.R
@@ -19,7 +19,7 @@ mod_home_page_ui <- function(id){
     fluidRow(align = 'center',
    box(width = 12,
        h1("kairos", align = 'center'),
-       column(width = 8, offset = 2, h4("kairos provides the NF community with analytical tools that enable consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Sapien faucibus et molestie ac feugiat. In tellus integer feugiat scelerisque varius morbi enim. Consectetur libero id faucibus nisl.", align = "center")),
+       column(width = 8, offset = 2, h4("kairos provides the NF community with analytical tools to enable high-level exploration of experimental results. Currently, we support exploration of genomics data, transfer learning on gene expression data, and dose response data. Have an idea, or want to see an analysis? Email us at nf-osi@sagebionetworks.org.", align = "center")),
        # column(width = 12, offset = 3,
        #        box(h2("read the docs"), 
        #            width = 2),
