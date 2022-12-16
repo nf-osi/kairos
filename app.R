b0d931df745d3a43a272db46c1dcf7f7630f9603
bhoff <bruce.hoff@sagebase.org>
2022-12-16 13:48:26 -0800

    install 'markdown' on app startup

--- app.R
+++ app.R
@@ -2,6 +2,19 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
+
+# For some reason 'markdown' fails to install into ShinyApps
+# so let's install it upon app' start up
+.onLoad <- function(libname, pkgname) {
+  if (Sys.getenv("R_CONFIG_ACTIVE") == "shinyapps") {
+  	if (nchar(system.file("", package="markdown"))==0) { # if package is not available...
+  		message("Installing 'markdown'")
+  		install.packages("markdown", repos = "http://cran.fhcrc.org") # ...then install it
+  	}
+  }
+}
+
+
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
 kairos::run_app() # add parameters here (if any)
